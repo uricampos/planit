c516c97839c6afae02cc4de9113106586e316469
Uriel Campos <urieldecampos@gmail.com>
2023-06-29 16:24:38 -0300

    Added css and reposive style for login and register page, also added login schema

--- client/src/Pages/Login/index.jsx
+++ client/src/Pages/Login/index.jsx
@@ -1,17 +1,98 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { useFormik } from 'formik';
+import { loginSchema } from '../Register/Schemas';
 
 import './Styles/styles.css';
 
+const onSubmit = (values, actions) => {
+    console.log('enviado!', values);
+    actions.resetForm();
+};
+
 function Login() {
+    const {
+        values,
+        errors,
+        touched,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+    } = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: loginSchema,
+        onSubmit,
+    });
+
     return (
         <div className="container-login">
             <div className="card-login">
-                <div className="form-login"></div>
-                <motion.div className="footer-login"
-                    initial={{opacity: 0}}
-                    animate={{opacity: 1}}
-                    transition={{duration: 1.5, ease: "easeInOut"}}
+                <div className="form-login">
+                    <form onSubmit={handleSubmit}>
+                        <div className="div-email-login">
+                            <label
+                                htmlFor="email"
+                                className="email-login-label"
+                            >
+                                Email
+                            </label>
+                            <input
+                                id="email"
+                                type="email"
+                                className={errors.email ? 'input-error' : ''}
+                                placeholder="Insira seu email"
+                                value={values.email}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            {errors.email && touched.email && (
+                                <p>{errors.email}</p>
+                            )}
+                        </div>
+                        <div className="div-password-login">
+                            <label
+                                htmlFor="password"
+                                className="password-login-label"
+                            >
+                                Password
+                            </label>
+                            <input
+                                id="password"
+                                type="password"
+                                className={errors.password ? 'input-error' : ''}
+                                placeholder="Insira sua senha"
+                                value={values.password}
+                                onChange={handleChange}
+                                onBlur={handleBlur}
+                            />
+                            {errors.password && touched.password && (
+                                <p>{errors.password}</p>
+                            )}
+                        </div>
+                        <div className="div-btn-login">
+                            <button
+                                type="submit"
+                                className="btn-login"
+                                disabled={
+                                    isSubmitting ||
+                                    errors.email ||
+                                    errors.password
+                                }
+                            >
+                                Login
+                            </button>
+                        </div>
+                    </form>
+                </div>
+                <motion.div
+                    className="footer-login"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1.5, ease: 'easeInOut' }}
                 >
                     <p>ou</p>
                     <p
--- client/src/Pages/Register/index.jsx
+++ client/src/Pages/Register/index.jsx
@@ -5,7 +5,7 @@ import './Styles/styles.css';
 import { registerSchema } from './Schemas';
 
 const onSubmit = (values, actions) => {
-    console.log('enviado!');
+    console.log('enviado!', values);
     actions.resetForm();
 };
 
@@ -31,100 +31,103 @@ function Register() {
 
     return (
         <div className="container-register">
-            <div className="card-register">
-                <div className="form-register">
-                    <form onSubmit={handleSubmit} autoComplete="off">
-                        <div className="div-register-name">
-                            <label htmlFor="name">Nome</label>
-                            <input
-                                id="name"
-                                type="text"
-                                placeholder="Nome completo/nome da empresa"
-                                value={values.name}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                className={
-                                    errors.name && touched.name
-                                        ? 'input-error'
-                                        : ''
-                                }
-                            />
-                        </div>
-                        {errors.name && touched.name && (
-                            <p className="error-paragraph">{errors.name}</p>
-                        )}
-                        <div className="div-register-email">
-                            <label htmlFor="email">Email</label>
-                            <input
-                                id="email"
-                                type="email"
-                                placeholder="Email"
-                                value={values.email}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                className={
-                                    errors.email && touched.email
-                                        ? 'input-error'
-                                        : ''
-                                }
-                            />
-                        </div>
-                        {errors.email && touched.email && (
-                            <p className="error-paragraph">{errors.email}</p>
-                        )}
-                        <div className="div-register-password">
-                            <label htmlFor="password">Password</label>
-                            <input
-                                id="password"
-                                type="password"
-                                placeholder="Senha"
-                                value={values.password}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                className={
-                                    errors.password && touched.password
-                                        ? 'input-error'
-                                        : ''
-                                }
-                            />
-                        </div>
-                        {errors.password && touched.password && (
-                            <p className="error-paragraph">{errors.password}</p>
-                        )}
-                        <div className="div-register-confirmPassword">
-                            <label htmlFor="confirmPassword">
-                                Confirm Password
-                            </label>
-                            <input
-                                id="confirmPassword"
-                                type="password"
-                                placeholder="Confirmar senha"
-                                value={values.confirmPassword}
-                                onChange={handleChange}
-                                onBlur={handleBlur}
-                                className={
-                                    errors.confirmPassword &&
-                                    touched.confirmPassword
-                                        ? 'input-error'
-                                        : ''
-                                }
-                            />
-                        </div>
-                        {errors.confirmPassword && touched.confirmPassword && (
-                            <p className="error-paragraph">
-                                {errors.confirmPassword}
-                            </p>
-                        )}
-                        <div className="div-register-btn">
-                            <button
-                                disabled={isSubmitting}
-                                className="btn-register-submit"
-                            >
-                                Registrar
-                            </button>
-                        </div>
-                    </form>
-                </div>
+            <div className="form-register">
+                <form onSubmit={handleSubmit} autoComplete="off">
+                    <div className="div-register-name">
+                        <label htmlFor="name">Nome</label>
+                        <input
+                            id="name"
+                            type="text"
+                            placeholder="Nome completo/nome da empresa"
+                            value={values.name}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className={
+                                errors.name && touched.name ? 'input-error' : ''
+                            }
+                        />
+                    </div>
+                    {errors.name && touched.name && (
+                        <p className="error-paragraph">{errors.name}</p>
+                    )}
+                    <div className="div-register-email">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            id="email"
+                            type="email"
+                            placeholder="Email"
+                            value={values.email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className={
+                                errors.email && touched.email
+                                    ? 'input-error'
+                                    : ''
+                            }
+                        />
+                    </div>
+                    {errors.email && touched.email && (
+                        <p className="error-paragraph">{errors.email}</p>
+                    )}
+                    <div className="div-register-password">
+                        <label htmlFor="password">Password</label>
+                        <input
+                            id="password"
+                            type="password"
+                            placeholder="Senha"
+                            value={values.password}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className={
+                                errors.password && touched.password
+                                    ? 'input-error'
+                                    : ''
+                            }
+                        />
+                    </div>
+                    {errors.password && touched.password && (
+                        <p className="error-paragraph">{errors.password}</p>
+                    )}
+                    <div className="div-register-confirmPassword">
+                        <label htmlFor="confirmPassword">
+                            Confirm Password
+                        </label>
+                        <input
+                            id="confirmPassword"
+                            type="password"
+                            placeholder="Confirmar senha"
+                            value={values.confirmPassword}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            className={
+                                errors.confirmPassword &&
+                                touched.confirmPassword
+                                    ? 'input-error'
+                                    : ''
+                            }
+                        />
+                    </div>
+                    {errors.confirmPassword && touched.confirmPassword && (
+                        <p className="error-paragraph">
+                            {errors.confirmPassword}
+                        </p>
+                    )}
+                    <div className="div-register-btn">
+                        <button
+                            type="submit"
+                            disabled={
+                                isSubmitting ||
+                                errors.name ||
+                                errors.email ||
+                                errors.password ||
+                                errors.confirmPassword
+                            }
+                            className="btn-register-submit"
+                        >
+                            Registrar
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     );
