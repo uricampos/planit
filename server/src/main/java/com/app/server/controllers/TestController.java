12382421668171fcb509216ec793c15129bed108
Pedro Crespan <pedrocrespan@hotmail.com>
2023-07-06 13:21:16 -0300

    Added OrderService

--- server/src/main/java/com/app/server/controllers/TestController.java
+++ server/src/main/java/com/app/server/controllers/TestController.java
@@ -1,14 +1,19 @@
 package com.app.server.controllers;
 
 import com.app.server.dto.UserDTO;
+import com.app.server.entities.Organization;
 import com.app.server.entities.UserLogin;
+import com.app.server.repositories.OrganizationRepository;
 import com.app.server.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class TestController {
@@ -16,27 +21,32 @@ public class TestController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private OrganizationRepository organizationRepository;
+
 
     @GetMapping(value = "/register")
-    public String register(Model model, UserDTO userDTO) {
+    public void register(Model model, UserDTO userDTO) {
         model.addAttribute("user", userDTO);
-        return "register";
     }
 
     @PostMapping(value = "/register")
-    public String saveRegister(@ModelAttribute("user") UserDTO userDTO) {
+    public void saveRegister(@ModelAttribute("user") UserDTO userDTO) {
         userService.save(userDTO);
-        return "register";
     }
 
     @GetMapping(value = "/login")
-    public String login(Model model, UserLogin userLogin) {
+    public void login(Model model, UserLogin userLogin) {
         model.addAttribute("userLogin", userLogin);
-        return "login";
     }
 
     @GetMapping(value = "/home")
     public String home() {
         return "home";
+    }
+
+    @GetMapping(value = "/organizations")
+    public ResponseEntity<List<Organization>> organizations() {
+        return ResponseEntity.ok().body(organizationRepository.findAll());
     }
 }
---
+++ server/src/main/java/com/app/server/services/OrderService.java
@@ -0,0 +1,23 @@
+package com.app.server.services;
+
+import com.app.server.entities.Order;
+import com.app.server.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public Order findById(Long id) {
+        return orderRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Order not found"));
+    }
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+}
