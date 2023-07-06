91b39ded98ef6922231a6c7c59064f6827299857
Pedro Crespan <pedrocrespan@hotmail.com>
2023-07-06 14:08:31 -0300

    Added ProductDTO

---
+++ server/src/main/java/com/app/server/dto/ProductDTO.java
@@ -0,0 +1,53 @@
+package com.app.server.dto;
+
+import com.app.server.entities.Product;
+import jakarta.persistence.Column;
+
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.description = product.getDescription();
+        this.price = product.getPrice();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
