3f6e046cc445c88cd3a358788781a176ac48186e
Pedro Crespan <pedrocrespan@hotmail.com>
2023-07-07 13:20:36 -0300

    Possible fix for cors

--- server/src/main/java/com/app/server/security/WebSecurityConfiguration.java
+++ server/src/main/java/com/app/server/security/WebSecurityConfiguration.java
@@ -9,11 +9,17 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -42,8 +48,7 @@ public class WebSecurityConfiguration {
                 .defaultSuccessUrl("/organizations", true)
                 .permitAll()
                 .and()
-                .cors()
-                .and()
+                .cors((Customizer<CorsConfigurer<HttpSecurity>>) corsConfigurationSource())
                 .logout()
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
@@ -56,5 +61,15 @@ public class WebSecurityConfiguration {
     @Bean
     public static PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
