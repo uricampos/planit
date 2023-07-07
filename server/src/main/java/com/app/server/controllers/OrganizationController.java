a7921543a4d38f6913c6cda42d0bddebb7879ee2
Pedro Crespan <pedrocrespan@hotmail.com>
2023-07-07 12:55:18 -0300

    Added cors on controllers

--- server/src/main/java/com/app/server/controllers/OrganizationController.java
+++ server/src/main/java/com/app/server/controllers/OrganizationController.java
@@ -5,15 +5,13 @@ import com.app.server.dto.OrganizationMinDTO;
 import com.app.server.services.OrganizationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/organizations")
+@CrossOrigin(origins = "http://localhost:5173")
 public class OrganizationController {
     @Autowired
     private OrganizationService organizationService;
