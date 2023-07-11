4498b54526447de1062b5bed7d9e9a8a56b65e5e
Pedro Crespan <pedrocrespan@hotmail.com>
2023-07-11 15:35:01 -0300

    Cors default configuration overriden

---
+++ server/src/main/java/com/app/server/security/CorsConfiguration.java
@@ -0,0 +1,15 @@
+package com.app.server.security;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("No Access-Control-Allow-Origin")
+                .allowCredentials(true);
+    }
+}
